5ec9ea46113e659ef9bf2edeef860d7422bc22da
Fabrício Araújo <fabaraujo23@gmail.com>
2021-07-10 11:14:11 -0300

    loops

---
+++ src/17-continue-keyword.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+    for (i in 0..4) {
+        if (i == 2) {
+            continue
+        }
+        println(i)
+    }
+
+    println()
+
+    myLoop@ for (i in 1..3) {
+        for (j in 1..3) {
+            if (i == 2 && j == 2) {
+                continue@myLoop
+            }
+            println ("$i $j")
+        }
+    }
+}
